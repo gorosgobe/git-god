01ed4adf6b60674ff1ae59cbe1f767a95aa6985f
lty16 <lty16@gmail.com>
2018-02-14 15:31:12 +0000

    Added nice log. Contributors: @tiimgreen.

---
+++ custom/git_log_nice.rb
@@ -0,0 +1,13 @@
+class GitIgnoreCommand < BaseCommand
+
+  require_relative '../lib/base_command'
+
+  def initialize
+    super("lg", Range.new(0))
+  end
+
+  def execute(_args)
+    system "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+  end
+
+end
